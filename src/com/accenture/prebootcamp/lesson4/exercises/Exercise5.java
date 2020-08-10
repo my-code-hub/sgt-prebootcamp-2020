4a7540622bb0b5dc1b10cff48e00fb2181ba5e0a
Ritvars Dortans <ritvars.dortans@neotech.lv>
2020-08-10 21:28:05 +0300

    Lesson 4 - exercise 5

---
+++ src/com/accenture/prebootcamp/lesson4/exercises/Exercise5.java
@@ -0,0 +1,37 @@
+package com.accenture.prebootcamp.lesson4.exercises;
+
+public class Exercise5 {
+
+    public static void main(String[] args) {
+        //a)
+        int a, b, c;
+        a = 0;
+
+        //b System.out.println('ab');
+        System.out.println("ab");
+
+        //c System.out.println( (3 + 2) – 1) );
+        System.out.println((3 + 2) - 1);
+
+        //d) int d = 3.2;
+        float d = 3.2f;
+
+        //e)
+        float e = 2.1f;
+        float f = e;
+
+        //f)
+        float g = 10f;
+        float h = 10.50f;
+        g = h;
+
+        //g)
+        int n;
+        {
+            n = 10;
+        }
+        n = 10;
+        System.out.println(n);
+
+    }
+}
