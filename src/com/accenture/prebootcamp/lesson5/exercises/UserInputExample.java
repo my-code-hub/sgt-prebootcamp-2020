06143abdf276d5c3d15cc9526bde3c38bedb4e59
Ritvars Dortans <ritvars.dortans@neotech.lv>
2020-08-11 21:21:35 +0300

    UserInputExample

---
+++ src/com/accenture/prebootcamp/lesson5/exercises/UserInputExample.java
@@ -0,0 +1,37 @@
+package com.accenture.prebootcamp.lesson5.exercises;
+
+import java.util.Scanner;
+
+public class UserInputExample {
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        int number = userInput.nextInt();
+        System.out.println(number);
+
+        System.out.println(userInput.nextInt());
+
+        // Getting float input
+        System.out.print("Enter float: ");
+        float myFloat = userInput.nextFloat();
+        System.out.println("Float entered = " + myFloat);
+
+        // Getting double input
+        System.out.print("Enter double: ");
+        double myDouble = userInput.nextDouble();
+        System.out.println("Double entered = " + myDouble);
+
+        // Getting String input
+        System.out.print("Enter text: ");
+        String myString = userInput.next();
+        System.out.println("Text entered = " + myString);
+
+        // Getting String input
+        System.out.print("Enter text: ");
+        String myString2 = userInput.nextLine();
+        System.out.println("Text entered = " + myString2);
+
+        userInput.close();
+    }
+}
