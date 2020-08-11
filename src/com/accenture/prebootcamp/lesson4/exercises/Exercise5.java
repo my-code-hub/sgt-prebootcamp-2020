9d92ab2ea730ac8c713fa6c39ed607e1a7244d08
Ritvars Dortans <ritvars.dortans@neotech.lv>
2020-08-11 19:18:07 +0300

    Lesson 5 - exercise 5 solution

--- src/com/accenture/prebootcamp/lesson4/exercises/Exercise5.java
+++ src/com/accenture/prebootcamp/lesson4/exercises/Exercise5.java
@@ -33,5 +33,43 @@ public class Exercise5 {
         n = 10;
         System.out.println(n);
 
+        //h
+        boolean boolean1, boolean2;
+        boolean1 = false;
+        boolean2 = true;
+
+        //i
+        int x;
+        x = 1;
+        int r = x + 1;
+
+        //j String s = 'This is a string';
+        String s = "This is a string";
+
+        //l) long number = 2000000000000000;
+        //      System.out.println(number);
+        //same as 2000_000_000_000_000L
+        long number = 2000_000_000_000_000L;
+        System.out.println(number);
+
+
+        //m) char myChar = "b";
+        //   System.out.println(myChar);
+        char myChar = 'b';
+        System.out.println(myChar);
+
+
+        //n) final int myNumber = 20;
+        //   myNumber++;
+        //   System.out.println(myNumber);
+        final int myNumber = 20;
+        //can't assign another value to final variable
+        //myNumber++;
+        System.out.println(myNumber);
+
+        //o) float myFloat = 0.5;
+        //   System.out.println(myFloat);
+        float myFloat = 0.5f;
+        System.out.println(myFloat);
     }
 }
