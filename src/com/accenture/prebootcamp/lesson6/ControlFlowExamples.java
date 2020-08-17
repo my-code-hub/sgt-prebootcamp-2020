d22f03137a79f21a227421a891ff5c197b4b028b
Ritvars Dortans <ritvars.dortans@neotech.lv>
2020-08-17 16:08:22 +0300

    ControlFlowExamples

---
+++ src/com/accenture/prebootcamp/lesson6/ControlFlowExamples.java
@@ -0,0 +1,41 @@
+package com.accenture.prebootcamp.lesson6;
+
+public class ControlFlowExamples {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        //if example
+        if (a && b) {
+            System.out.println("a && b is true");
+        }
+
+        System.out.println("******************");
+
+        //if else example
+        if (a && b) {
+            System.out.println("a and b is true");
+        } else {
+            System.out.println("a or b is not true");
+        }
+
+        System.out.println("******************");
+
+        a = true;
+        b = false;
+        boolean c = true;
+        //if, else if, else example
+        if (a && b && c) {
+            System.out.println("a and b and c is true");
+        } else if (a) {
+            System.out.println("a is true");
+        } else if (b) {
+            System.out.println("b is true");
+        } else if (c) {
+            System.out.println("c is true");
+        } else {
+            System.out.println("none of them are true");
+        }
+    }
+}
