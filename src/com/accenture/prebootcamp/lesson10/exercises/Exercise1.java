ee75b6ed4b5b81d7f29682086f36b4d31991e245
Ritvars Dortans <ritvars.dortans@neotech.lv>
2020-08-24 20:19:56 +0300

    Exercise 1 solution (lesson 10 - arrays)

---
+++ src/com/accenture/prebootcamp/lesson10/exercises/Exercise1.java
@@ -0,0 +1,21 @@
+package com.accenture.prebootcamp.lesson10.exercises;
+
+//1. declare and initialize array with six arbitrary whole numbers. Write a code which:
+//    1) prints out all of the numbers from array
+//    2) prints out all of the numbers from array in reverse order
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 3, 5, 2, 5, 7};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+
+        System.out.println();
+
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
