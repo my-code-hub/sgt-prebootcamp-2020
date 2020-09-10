4697e5e8d6721d63e906062444db210e6331281a
Ritvars Dortans <ritvars.dortans@neotech.lv>
2020-09-10 21:25:26 +0300

    solution exercise 1 - encapsulation

--- src/com/accenture/prebootcamp/encapsulation/excersises/exercise1/p1/A.java
+++ src/com/accenture/prebootcamp/encapsulation/excersises/exercise1/p1/A.java
@@ -1,35 +1,77 @@
 package com.accenture.prebootcamp.encapsulation.excersises.exercise1.p1;
 
 //1. Change access modifier for this class so the class would be accessible from any package
-class A {
+public class A {
 
     //2. encapsulate this property so it's accessible only via getter method.
     // This getter method should be accessible from class BaseApp and class InnerApp
     // it shouldn't be possible to change this value from BaseApp
-    public int a = 5;
+    private int a = 5;
+
+    public int getA() {
+        return a;
+    }
 
     //3. change access modifier and encapsulate this property so it's only accessible in scope of
     // this package via getter method and can't be modified
-    public int b = 2;
+    private int b = 2;
+
+    int getB() {
+        return b;
+    }
 
     //4. change access modifier for this property so it can be accessed from any class in any package
     // (via getter method), but changed only via setter method only inside of this package
     private String c = "c";
 
+    void setC(String c) {
+        this.c = c;
+    }
+
+    public String getC() {
+        return c;
+    }
+
     // change access modifier for this property so it can be accessed and modified
     // from any class in any package directly (without any getter/setter methods)
-    private String d = "d";
+    public String d = "d";
 
     // create method "secret" which can only be used inside of this object. Calling this method
     // should print out message "secret()". Then create a default constructor for this class
     // (make sure the constructor also has access modifier which is accessible from anywhere)
     // and call this "secret" method inside the constructor.
+    public A() {
+        secret();
+    }
+
+    private void secret() {
+        System.out.println("secret()");
+    }
 
     //Create method "local" with package scope. (it should output message "local()".
 
+    void local() {
+        System.out.println("local()");
+    }
+
     //create publicly accessible method "famous" which outputs text "famous()"
+
+    public void famous() {
+        System.out.println("famous()");
+    }
 
     //Bonus!:
     // create three overloaded constructors (choose any parameters you like) each with different
     // access modifier
+    public A(long number) {
+        System.out.println("number: " + number);
+    }
+
+    A(String text) {
+        System.out.println("text: " + text);
+    }
+
+    private A(boolean trueOrFalse) {
+        System.out.println("trueOrFalse: " + trueOrFalse);
+    }
 }
--- src/com/accenture/prebootcamp/encapsulation/excersises/exercise1/p1/InnerApp.java
+++ src/com/accenture/prebootcamp/encapsulation/excersises/exercise1/p1/InnerApp.java
@@ -5,32 +5,46 @@ public class InnerApp {
     public static void main(String[] args) {
 
         //1. create new instance of class A using default constructor
+        A objectA = new A();
 
         //2. output the value of property "a" via the getter method
         //Note: if you did everything right, then the code below shouldn't be compiling:
         //A object = new A();
         //System.out.println(object.a);
+        System.out.println(objectA.getA());
 
         //3. output the value of property "b" via the getter method
         //Note: if you did everything right, then the code below shouldn't be compiling:
         // A object2 = new A();
         // System.out.println(object2.b);
+        System.out.println(objectA.getB());
 
         //4. output the value of property c
         //modify the value of property c to "Updated C!"
         //output the value of property c
+        System.out.println(objectA.getC());
+        objectA.setC("Updated C!");
+        System.out.println(objectA.getC());
 
         //5. output property value "d"
         // update property "d" to value "InnerApp"
         // output property value "d"
+        System.out.println(objectA.d);
+        objectA.d = "InnerApp";
+        System.out.println(objectA.d);
 
         //6. call method "local()"
+        objectA.local();
 
         //7. call method "famous()"
+        objectA.famous();
 
         //8. can you call method "secret" on this object? Why not?
+       // objectA.secret();
 
         //9.create new instance of class B using default constructor
         // why can you do it in this class, but couldn't do it in class "BaseApp"?
+
+        B objectB = new B();
     }
 }
