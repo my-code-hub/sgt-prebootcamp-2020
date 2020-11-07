efc7bed277a3d52f7be5940b8574963cf7865869
Ritvars Dortans <ritvars.dortans@neotech.lv>
2020-11-07 15:01:03 +0200

    array list exercises

---
+++ src/com/accenture/bootcamp/collections/exercises/Exercise30.java
@@ -0,0 +1,19 @@
+package com.accenture.bootcamp.collections.exercises;
+
+public class Exercise30 {
+
+    public static void main(String[] args) {
+        //TODO
+        // 1. declare and initialize String array with 7 arbitrary colors.
+        // 2. Create new list "colors" (ArrayList) of type String
+        // 3. Copy/add all elements from String array to colors list
+        // 4. Remove 3rd element from colors list
+        // 5. Add new color at the end of the colors list
+        // 6. Add new color at the beginning of the colors list
+        // 7. Loop trough colors list and output all values (use for each loop)
+        // 8. Create new list (colorsSublist) which will be sublist of the colors list (will contain elements from
+        // index 2 to index 5)
+        //    (hint: take a look at List interface and find a method which you can use to acchieve this)
+        // 9. Use for loop to output each element of colorsSublist list
+    }
+}
