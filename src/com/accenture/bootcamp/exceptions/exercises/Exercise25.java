83f70af3d35c66980a70975be4b7e8cc2babde84
Ritvars Dortans <ritvars.dortans@neotech.lv>
2020-11-07 11:09:43 +0200

    Exercise 25 for exceptions

---
+++ src/com/accenture/bootcamp/exceptions/exercises/Exercise25.java
@@ -0,0 +1,32 @@
+package com.accenture.bootcamp.exceptions.exercises;
+
+//TODO 1. Create your own two exceptions
+// and name them EvenNumberException (checked)
+// and OddNumberException (unchecked)
+public class Exercise25 {
+
+    public static void main(String[] args) {
+        checkNumber(1);
+        checkNumber(2);
+    }
+
+    //TODO
+    //  3. call method evenOrOdd and pass it the number argument.
+    //  4. Implement error handling code in this method.
+    //     a) If method evenOrOdd throws EvenNumberException,
+    //        then you have to output text:
+    //             [number] is even!
+    //     b) If method evenOrOdd throws OddNumberException,
+    //        then you have to output text:
+    //             [number] is odd!
+    static void checkNumber(int number) {
+
+    }
+
+    //TODO 2. this method has to throw
+    // a) EvenNumberException exception if number is even value
+    // b) OddNumberException if number is odd value
+    static void evenOrOdd(int number) {
+
+    }
+}
