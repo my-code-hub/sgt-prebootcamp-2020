9a44ba936a8fd9bd73db7604cd208e41e442846b
Ritvars Dortans <ritvars.dortans@neotech.lv>
2020-11-10 18:22:09 +0200

    Exercise 32 adn 33 (set and map collections)

---
+++ src/com/accenture/bootcamp/collections/exercises/Exercise32.java
@@ -0,0 +1,11 @@
+package com.accenture.bootcamp.collections.exercises;
+
+public class Exercise32 {
+
+    //TODO Write a program which counts how many unique words are in text (see class Text, variable TEXT)
+    //Think of how you could use any of the Set implementation to implement
+    //this program.
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/accenture/bootcamp/collections/exercises/Exercise33.java
@@ -0,0 +1,12 @@
+package com.accenture.bootcamp.collections.exercises;
+
+//TODO Write a program which calculates the count of each word in
+// text (see class Text, variable TEXT)
+// Think of how you could use any of the Map implementations to implement
+// this program.
+public class Exercise33 {
+
+    public static void main(String[] args) {
+
+    }
+}
