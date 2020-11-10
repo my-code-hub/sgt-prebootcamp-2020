42120d33abc40e25ab0af411e2eb1654fc15e263
Ritvars Dortans <ritvars.dortans@neotech.lv>
2020-11-10 20:58:16 +0200

    exercise 32 solutions

--- src/com/accenture/bootcamp/collections/exercises/Exercise32.java
+++ src/com/accenture/bootcamp/collections/exercises/Exercise32.java
@@ -1,6 +1,7 @@
 package com.accenture.bootcamp.collections.exercises;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -12,12 +13,45 @@ public class Exercise32 {
     //this program.
 
     // Bonus:
-        // Ignore case sensitivity. For example, Apple and apple should be counted as the same word.
-
+    // Ignore case sensitivity. For example, Apple and apple should be counted as the same word.
 
     public static void main(String[] args) {
+        solutionExample1();
 
+        solutionExample2();
+
+        solutionExample3();
     }
 
+    static void solutionExample1() {
+        String[] words = Text.TEXT.toLowerCase().split("\\s+");
 
+        Set<String> uniqueWords = new HashSet<>();
+        for (String word : words) {
+            uniqueWords.add(word);
+        }
+
+        System.out.println("unique words: " + uniqueWords.size());
+    }
+
+    static void solutionExample2() {
+        String[] words = Text.TEXT.split("\\s+");
+
+        List<String> wordsArray = Arrays.asList(words);
+        Set<String> uniqueWords = new HashSet<>(wordsArray);
+
+        System.out.println("unique words: " + uniqueWords.size());
+    }
+
+    static void solutionExample3() {
+        String[] words = Text.TEXT.split("\\s+");
+
+        Set<String> uniqueWords = new HashSet<>();
+        uniqueWords.addAll(Arrays.asList(words));
+
+        //another approach
+        //Collections.addAll(uniqueWords, words);
+
+        System.out.println("unique words: " + uniqueWords.size());
+    }
 }
